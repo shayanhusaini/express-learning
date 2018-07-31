35fc11a05dab33a948c168eaf8559d2fcc90c209
Shayan Husaini <shayanhusaini@gmail.com>
2018-07-31 17:26:28 +0500

    Skeleton with one route

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+// Middlewares
+app.use(logger('dev'));
+
+// Routes
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'You requested index page'
+    });
+});
+
+// Catch 404 Errors and forward them to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Error handler function
+app.use((err, req, res, next) => {
+    const error = app.get('env') === 'development' ? err : {};
+    const status = err.status || 500;
+
+    // Response to client
+    res.status(status).json({
+        error: {
+            message: error.message
+        }
+    });
+
+    // Response to ourselves
+    console.error(err);
+});
+
+
+// Start the server
+const port = app.get('port') || 3000;
+app.listen(port, () => console.log('Server is listening on port '+ port));
