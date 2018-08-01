9ccb86aa927a32cfef7083b9bda45d867b62cd28
Shayan Husaini <shayanhusaini@gmail.com>
2018-08-01 18:34:51 +0500

    Added validation

--- controllers/users.js
+++ controllers/users.js
@@ -7,36 +7,35 @@ module.exports = {
         res.status(200).json(users);
     },
     newUser: async (req, res, next) => {
-        const newUser = new User(req.body);
+        const newUser = new User(req.value.body);
         const user = await newUser.save();
         res.status(201).json(user);
     },
     getUser: async (req, res, next) => {
-        const { userId } = req.params;
+        const { userId } = req.value.params;
         const user = await User.findById(userId);
         res.status(200).json(user);
-
     },
     replaceUser: async (req, res, next) => {
-        const {userId} = req.params;
-        const newUser = req.body;
+        const {userId} = req.value.params;
+        const newUser = req.value.body;
         const result = await User.findByIdAndUpdate(userId, newUser);
         res.status(200).json({success: true});
     },
     updateUser: async (req, res, next) => {
-        const {userId} = req.params;
-        const newUser = req.body;
+        const {userId} = req.value.params;
+        const newUser = req.value.body;
         const result = await User.findByIdAndUpdate(userId, newUser);
         res.status(200).json({success: true});
     },
     getUserCars: async (req, res, next) => {
-        const {userId} = req.params;
+        const {userId} = req.value.params;
         const user = await User.findById(userId).populate('cars');
         res.status(200).json(user.cars);
     },
     newUserCar: async (req, res, next) => {
-        const {userId} = req.params;
-        const newCar = new Car(req.body);
+        const {userId} = req.value.params;
+        const newCar = new Car(req.value.body);
         const user = await User.findById(userId);
         newCar.seller = user;
         await newCar.save();
