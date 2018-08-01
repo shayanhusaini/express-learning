cd0c1f76eda1006835c84903961ec210904eb9aa
Shayan Husaini <shayanhusaini@gmail.com>
2018-08-01 14:19:57 +0500

    Included express promise router and use of async await

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,13 @@
 const User = require('../models/user');
 
 module.exports = {
-    index: (req, res, next) => {
-        User.find({})
-            .then(users => {
-                res.status(200).json(users);
-            })
-            .catch(err => {
-                next(err);
-            });
-
+    index: async (req, res, next) => {
+        const users = await User.find({});
+        res.status(200).json(users);
     },
-    newUser: (req, res, next) => {
+    newUser: async (req, res, next) => {
         const newUser = new User(req.body);
-        newUser.save()
-            .then(user => {
-                res.status(201).json(user);
-            })
-            .catch(err => {
-                next(err);
-            });
+        const user = await newUser.save();
+        res.status(201).json(user);
     }
 };
